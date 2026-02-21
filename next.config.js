bcdb11604066ca6489a7925b81d220928b84a769
ami228vx <shamshotova@icloud.com>
2026-02-21 20:51:00 +0500

    Add next.config.js file with Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=60, stale-while-revalidate=120',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
